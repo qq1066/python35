ca3d72d081c1f7e3c4ebf27feade779cf3b2bd40
qq1066 <1066875821@qq.com>
2017-07-10 20:22:33 +0800

    Initial commit

---
+++ Test/T000.py
@@ -0,0 +1,14 @@
+# s1 = "fajkfakrjiofasfafkafhiaifafanfnafafkakfjajfafjafjajfierfaifqapfapjkafja"
+# l1 = list(s1)
+# l1.sort()
+# j = l1[0]
+# cstr = 0
+# for i in l1:
+#     if i == j:
+#         cstr += 1
+#         # print(cstr, i)
+#     else:
+#         print(j, cstr, end=' ')
+#         cstr = 0
+#         cstr += 1
+#         j = i
